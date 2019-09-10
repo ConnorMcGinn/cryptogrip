9795d8bac95b04e6d7a944c9dc37d1e1174ba727
Connor McGinn <connor.mcginn@gmail.com>
2019-09-10 15:57:43 -0500

    Initial commit - project skeleton and test API call only

---
+++ src/main/java/org/launchcode/cryptogrip/CryptogripApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.cryptogrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptogripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptogripApplication.class, args);
+	}
+
+}
